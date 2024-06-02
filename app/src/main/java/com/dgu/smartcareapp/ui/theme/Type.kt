15ead33d0913277dbf2f47e869ff28ec634d1c00
Dan2dani <eldms0406@naver.com>
2024-06-02 22:33:13 +0900

    [init] 타이포그라피, 컬러 추가

--- app/src/main/java/com/dgu/smartcareapp/ui/theme/Type.kt
+++ app/src/main/java/com/dgu/smartcareapp/ui/theme/Type.kt
@@ -1,34 +1,87 @@
 package com.dgu.smartcareapp.ui.theme
 
-import androidx.compose.material3.Typography
+import android.content.res.Resources.Theme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dgu.smartcareapp.R
+import org.w3c.dom.Text
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val FontFamily = FontFamily(
+    Font(R.font.petandard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold)
 )
+
+
+//폰트 패밀리가 폰트웨이트에 따라 알아서 선택이 된다.
+data class SCATypography(
+    val semiBold24: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 38.sp,
+        fontSize = 24.sp
+    ),
+    val semiBold20: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 32.sp,
+        fontSize = 20.sp
+    ),
+    val semiBold16: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 32.sp,
+        fontSize = 16.sp
+    ),
+    val semiBold14: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 32.sp,
+        fontSize = 14.sp
+    ),
+    val regular16: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 32.sp,
+        fontSize = 16.sp
+    ),
+    val regular14: TextStyle = TextStyle(
+        fontFamily = com.dgu.smartcareapp.ui.theme.FontFamily,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 32.sp,
+        fontSize = 14.sp
+    ),
+)
+
+@Composable
+fun semiBold24(): TextStyle {
+    return SmartCardAppTheme.typography.semiBold24
+}
+
+@Composable
+fun semiBold20(): TextStyle {
+    return SmartCardAppTheme.typography.semiBold20
+}
+
+@Composable
+fun semiBold16(): TextStyle {
+    return SmartCardAppTheme.typography.semiBold16
+}
+
+@Composable
+fun semiBold14(): TextStyle {
+    return SmartCardAppTheme.typography.semiBold14
+}
+
+@Composable
+fun regular16(): TextStyle {
+    return SmartCardAppTheme.typography.regular16
+}
+
+@Composable
+fun regular14(): TextStyle {
+    return SmartCardAppTheme.typography.regular14
+}
