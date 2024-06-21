7b79fb1f95e7646394c4a75569e4e36ef360aae7
Dan2dani <eldms0406@naver.com>
2024-06-21 09:45:28 +0900

    [feat] 알람 관련 util 구현

---
+++ app/src/main/java/com/dgu/smartcareapp/alarm/AlarmUtils.kt
@@ -0,0 +1,63 @@
+package com.dgu.smartcareapp.alarm
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+import android.util.Log
+import java.util.Calendar
+
+/**
+ * 알람 관련 utils
+ */
+class AlarmUtils(private val context: Context) {
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    fun setAlarm(hour: Int, minute: Int, toDoTitle: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (!alarmManager.canScheduleExactAlarms()) {
+                val intent = Intent(ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
+                context.startActivity(intent)
+                return
+            }
+        }
+
+        val intent = Intent(context, AlarmReceiver::class.java)
+            .apply {
+                action = "com.dgu.smartcareapp.ALARM_ACTION"
+                putExtra(TODO_TITLE, toDoTitle)
+            }
+
+        val alarmIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+
+            if (before(Calendar.getInstance())) {
+                add(Calendar.DATE, 1)
+            }
+        }
+
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            alarmIntent
+        )
+
+        Log.d("dana", "알람이 설정되었습니다: ${calendar.time}")
+    }
+
+    companion object {
+        const val TODO_TITLE = "todoTitle"
+    }
+}
